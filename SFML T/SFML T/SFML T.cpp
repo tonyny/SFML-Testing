e02d971b40e59999214ed394ff69cd19c80a7c51
tonyny <tony.nguy@live.com>
2017-01-31 23:02:06 +1100

    created window and event loop

--- SFML T/SFML T/SFML T.cpp
+++ SFML T/SFML T/SFML T.cpp
@@ -2,10 +2,22 @@
 //
 
 #include "stdafx.h"
-
+#include "SFML/Graphics.hpp"
+#include "SFML/Window.hpp"
 
 int main()
 {
-    return 0;
-}
+	sf::RenderWindow window(sf::VideoMode(800, 600), "PLEASE HELP ME!");
 
+	while (window.isOpen())
+	{
+		sf::Event e;
+		while (window.pollEvent(e))
+		{
+			if (e.type == sf::Event::Closed)
+				window.close();
+		}
+	}
+
+	return 0;
+}
